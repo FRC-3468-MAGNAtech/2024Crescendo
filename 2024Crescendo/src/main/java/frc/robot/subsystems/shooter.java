3063a907184f2d8eda9bda01692e2c936dd7973c
Jeremy Bauer <48760451+cablebauer@users.noreply.github.com>
2024-01-27 16:42:03 -0600

    saturday meeting

---
+++ 2024Crescendo/src/main/java/frc/robot/subsystems/shooter.java
@@ -0,0 +1,64 @@
+package frc.robot.subsystems;
+
+public class shooter {
+    //The shooter needs to run the motors at a certain speed when called upon
+    // It will need to shoot at two different speeds.  1 is slower for scoring in the amp, one is faster for scoring in the speaker
+    // it will have two brushless NEO motors.  
+    // it will have two SPARKMax controllers
+    // It has two belts.  
+// data types
+    //integer - negative and positive whole numbers
+    //double - decimal
+    //string - letters,numbers, and characters that have no numeric value
+    //char - 1 character
+    //array - list of strings in a reserved set of space.  
+    //boolean - 
+
+    //Things I have to know
+    // gear ratio
+    // diameter of the wheels that shoot the note
+    // Distance to speaker
+
+    //variables 
+    // rotation speed
+    // distance to the speaker ??? 
+    // control input on/off 
+    // PID??? 
+    // shootSpeakerSpeed double 
+    // shootAmpSpeed double 
+
+
+
+
+    //constants
+    // gear ratio
+    // wheel diameter
+    // 
+
+
+    //methods
+
+    // ShootSpeaker (High speed shot)
+//1. recieve input from button push
+//2. turn on motor
+    //i. use shootSpeaker to set speed
+    //ii. turn on motor
+//3. allow to run for specific amount of time
+    //i. add interrupt for external controller override
+//4. turn off 
+
+    // OoozeAmp (Low speed shot)
+    //1. recieve input from button push
+//2. turn on motor
+    //i. use ShootAmpSpeed to set speed
+    //ii. turn on motor
+//3. allow to run for specific amount of time
+    //i. add interrupt for external controller override
+//4. turn off 
+
+
+
+
+
+
+}

f229b840acffb60b80528fb38274db240083f895
RandomChristianUsername <fantasynstuff404@gmail.com>
2024-02-03 16:01:44 -0600

    yes

--- 2024Crescendo/src/main/java/frc/robot/RobotContainer.java
+++ 2024Crescendo/src/main/java/frc/robot/RobotContainer.java
@@ -8,8 +8,10 @@ import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 /**
@@ -20,6 +22,12 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
+
+  private final JoystickButton home = new JoystickButton(
+    primaryDriver,
+    XboxController.Button.kX.value);
+  
+
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
